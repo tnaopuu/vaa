10977a7f3ff6364c946532f74c4e063139184974
heiroa <hlaille.pro@gmail.com>
2021-04-21 14:20:19 -1000

    init

---
+++ model.php
@@ -0,0 +1,18 @@
+<?php
+    function getAdherents()
+    {
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=vaa;charset=utf8', 'root', 'root');
+        }
+        
+        catch(Exception $e)
+        {
+            die('Erreur : '.$e->getMessage());
+        }
+
+        $req = $bdd->query('SELECT id, nom, prenom FROM membre');
+
+        return $req;
+    }
+?>
